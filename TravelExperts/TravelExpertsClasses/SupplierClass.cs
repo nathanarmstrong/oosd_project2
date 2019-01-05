708444b3836e29e5cfda6b0f56ab000921c55b83
jhsharpe <44778959+jhsharpe@users.noreply.github.com>
2019-01-05 12:36:35 -0700

    created packages class

--- TravelExperts/TravelExpertsClasses/SupplierClass.cs
+++ TravelExperts/TravelExpertsClasses/SupplierClass.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TravelExpertsClasses
 {
-    class Class2
+    class SupplierClass
     {
     }
 }
--- TravelExperts/TravelExpertsClasses/TravelPackagesClass.cs
+++ TravelExperts/TravelExpertsClasses/TravelPackagesClass.cs
@@ -9,5 +9,49 @@ namespace TravelExpertsClasses
     // Travel Packages Class
     public class TravelPackagesClass
     {
+        // Private data for packages class
+        private string pkgName;
+        private string pkgStartDate;
+        private string pkgEndDate;
+        private string pkgDesc;
+        private decimal pkgBasePrice;
+        private decimal pkgAgencyCommission;
+
+        // Public getters and setters
+        public string PkgName
+        {
+            get { return pkgName; }
+            set { pkgName = value; }
+        }
+
+        public string PkgStartDate
+        {
+            get { return pkgStartDate; }
+            set { pkgStartDate = value; }
+        }
+
+        public string PkgEndDate
+        {
+            get { return pkgEndDate; }
+            set { pkgEndDate = value; }
+        }
+
+        public string PkgDesc
+        {
+            get { return pkgDesc; }
+            set { pkgDesc = value; }
+        }
+
+        public decimal PkgBasePrice
+        {
+            get { return pkgBasePrice; }
+            set { pkgBasePrice = value; }
+        }
+
+        public decimal PkgAgencyCommission
+        {
+            get { return pkgAgencyCommission; }
+            set { pkgAgencyCommission = value; }
+        }
     }
 }
